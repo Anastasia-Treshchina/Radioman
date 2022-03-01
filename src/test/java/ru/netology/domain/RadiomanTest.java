02858c55f1004a6792149b648b786bf99067cc1b
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-03-02 00:35:29 +0400

    Format

---
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -0,0 +1,41 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadiomanTest {
+
+    @Test
+    void getCurrentStation() {
+
+    }
+
+    @Test
+    void setCurrentStation() {
+    }
+
+    @Test
+    void nextStation() {
+    }
+
+    @Test
+    void previousStation() {
+    }
+
+    @Test
+    void getCurrentVolume() {
+    }
+
+    @Test
+    void setCurrentVolume() {
+    }
+
+    @Test
+    void increaseVolume() {
+    }
+
+    @Test
+    void decreaseVolume() {
+    }
+}
