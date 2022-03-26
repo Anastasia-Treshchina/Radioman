c2441cd75957cd083a746187d3fdded655ec46ab
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-03-26 10:35:11 +0400

    Add

--- src/test/java/ru/netology/domain/RadiomanTest.java
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -5,11 +5,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadiomanTest {
-
     Radioman radio = new Radioman();
+
+
+    @Test
+    void shouldSetStation() {
+        Radioman radio = new Radioman(20);
+        radio.setCurrentStation(15);
+        assertEquals(15, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldSetStation20() {
+        Radioman radio = new Radioman(20);
+        radio.setCurrentStation(20);
+        assertEquals(0, radio.getCurrentStation());
+    }
 
     @Test
     void shouldChangeStation() {
+        Radioman radio = new Radioman();
         assertEquals(0, radio.getCurrentStation());
         radio.setCurrentStation(5);
         assertEquals(5, radio.getCurrentStation());
@@ -17,18 +32,21 @@ class RadiomanTest {
 
     @Test
     void shouldSetStationUnderMinStation() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(-1);
         assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     void shouldSetStationOverMaxStation() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(10);
         assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     void shouldNextStation() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(6);
         radio.nextStation();
         assertEquals(7, radio.getCurrentStation());
@@ -36,6 +54,7 @@ class RadiomanTest {
 
     @Test
     void shouldOverMaxStation1() {
+        Radioman radio = new Radioman(10);
         radio.setCurrentStation(9);
         radio.nextStation();
         assertEquals(0, radio.getCurrentStation());
@@ -43,6 +62,7 @@ class RadiomanTest {
 
     @Test
     void shouldOverMaxStation2() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(10);
         radio.nextStation();
         assertEquals(1, radio.getCurrentStation());
@@ -50,6 +70,7 @@ class RadiomanTest {
 
     @Test
     void shouldPrevStation() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(4);
         radio.previousStation();
         assertEquals(3, radio.getCurrentStation());
@@ -57,6 +78,7 @@ class RadiomanTest {
 
     @Test
     void shouldBelowMinStation1() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(0);
         radio.previousStation();
         assertEquals(9, radio.getCurrentStation());
@@ -64,13 +86,23 @@ class RadiomanTest {
 
     @Test
     void shouldBelowMinStation2() {
+        Radioman radio = new Radioman();
         radio.setCurrentStation(-1);
         radio.previousStation();
         assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
+    void shouldBelowMinStation3() {
+        Radioman radio = new Radioman(20);
+        radio.setCurrentStation(0);
+        radio.previousStation();
+        assertEquals(19, radio.getCurrentStation());
+    }
+
+    @Test
     void shouldChangeVolume() {
+        Radioman radio = new Radioman();
         assertEquals(0, radio.getCurrentVolume());
         radio.setCurrentVolume(5);
         assertEquals(5, radio.getCurrentVolume());
@@ -78,6 +110,7 @@ class RadiomanTest {
 
     @Test
     void shouldIncreaseVolume() {
+        Radioman radio = new Radioman();
         radio.setCurrentVolume(9);
         radio.increaseVolume();
         assertEquals(10, radio.getCurrentVolume());
@@ -85,20 +118,31 @@ class RadiomanTest {
 
     @Test
     void shouldOverMaxVolume1() {
-        radio.setCurrentVolume(10);
+        Radioman radio = new Radioman();
+        radio.setCurrentVolume(100);
         radio.increaseVolume();
-        assertEquals(10, radio.getCurrentVolume());
+        assertEquals(100, radio.getCurrentVolume());
     }
 
     @Test
     void shouldOverMaxVolume2() {
-        radio.setCurrentVolume(11);
+        Radioman radio = new Radioman();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldOverMaxVolume3() {
+        Radioman radio = new Radioman();
+        radio.setCurrentVolume(101);
         radio.increaseVolume();
         assertEquals(1, radio.getCurrentVolume());
     }
 
     @Test
     void shouldDecreaseVolume() {
+        Radioman radio = new Radioman();
         radio.setCurrentVolume(6);
         radio.decreaseVolume();
         assertEquals(5, radio.getCurrentVolume());
@@ -106,6 +150,7 @@ class RadiomanTest {
 
     @Test
     void shouldBelowMinVolume1() {
+        Radioman radio = new Radioman();
         radio.setCurrentVolume(0);
         radio.decreaseVolume();
         assertEquals(0, radio.getCurrentVolume());
@@ -113,6 +158,7 @@ class RadiomanTest {
 
     @Test
     void shouldBelowMinVolume2() {
+        Radioman radio = new Radioman();
         radio.setCurrentVolume(-1);
         radio.decreaseVolume();
         assertEquals(0, radio.getCurrentVolume());
